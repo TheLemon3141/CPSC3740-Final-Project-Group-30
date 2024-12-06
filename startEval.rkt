95b6a5b20955a576487e7d779ead692642cd78a5
TheLemon3141 <ejanvik@gmail.com>
2024-12-06 12:54:44 -0700

    Update startEval.rkt
    Created functions to make an environment (a list of name-value pairs)

--- startEval.rkt
+++ startEval.rkt
@@ -1,64 +1,185 @@
 #lang racket
 
-;Interpreter for a subset of racket.
+(define (startEval expr)
+    (evalExpr expr '()))
 
-(define (startEval prog)
-  (if (pair? prog);If it's a pair, execute as normal...
-;+ operation
-  (if (equal? (car prog) '+)                                       
-      (+ (list-ref prog 1) (list-ref prog 2));if true
-      ;if false, do the rest of the program
-;- operation
-  (if (equal? (car prog) '-)                                       
-      (- (list-ref prog 1) (list-ref prog 2));if true
-      ;if false, do the rest of the program
-;* operation
-  (if (equal? (car prog) '*)
-      (* (list-ref prog 1) (list-ref prog 2));if true
-      ;if false, do the rest of the program
-;/ operation (with divide-by-zero checking
-  (if (equal? (car prog) '/)                                       
-      (if (equal? (list-ref prog 2) 0);If they're trying to divide by 0
-          '"FOOL! YOU CAN'T DIVIDE BY ZERO"
-          (/ (list-ref prog 1) (list-ref prog 2));if true
-      );End of if list-ref 2 = 0
-;/ #t
-  (if (equal? (car prog) '#t)
-      #t
-      ;If not, do the rest of the program
-;/ #f
-  (if (equal? (car prog) '#f)
-      #f
-      ;If not, do the rest of the program
-;/ equal?
-  (if (equal? (car prog) 'equal?)
-      (if (equal? (list-ref prog 1) (list-ref prog 2))
-          (startEval #t)
-          (startEval #f)
-       )
-      ;If not, do the rest of the program
-;/ if (needs work)
-  (if (equal? (car prog) 'if)
-      (if (car(cdr prog))
-          (startEval (list-ref (cdr prog)) 1)
-          (startEval (list-ref (cdr prog)) 2)
+(define (evalExpr expr env)
+    (cond
+        ;; Number
+        [(number? expr)
+            expr]
+        ;; Variable
+        [(symbol? expr)
+            (lookup expr env)]
+        ;; Quoted
+        [(and (list? expr) (eq? (car expr) 'quote)) 
+            (second expr)]
+        ;; Arithmetic operators
+        [(and (list? expr) (member (car expr) '(+ - * /)))
+            (evalArithmetic expr env)]
+        ;; Relational operators
+        [(and (list? expr) (member (car expr) '(= <= < >= > equal?)))
+            (evalRelational expr env)]
+        ;; Lists
+        [(and (list? expr) (member (car expr) '(car cdr cons pair?)))
+            (evalList expr env)]
+        ;; Conditional
+        [(and (list? expr) (eq? (car expr) 'if))
+            (evalIf expr env)]
+        ;; Lambda expression
+        [(and (list? expr) (eq? (car expr) 'lambda))
+            (makeLambda (second expr) (third expr) env)]
+        ;; Function application
+        [(list? expr)
+            (apply (car expr) (cdr expr) env)]
+        ;; Local binding
+        [(and (list? expr) (member (car expr) '(let letrec)))
+            (evalBinding expr env)]
+        ;; Else
+        [else (error "Unknown expression: " expr)]))
+
+;; Lookup variable
+(define (lookup var env)
+    (let ((val (assoc var env)))    ;Finds the first pair in env whose car = var. Returns false if it can't find one.
+        (if val (cdr val) (error "Unknown variable: " var))))
+
+;; Local binding
+(define (evalBinding func args)   ;(evalBinding expr env)]
+  ('"Not defined yet")
+  ;Add args to env,
+  ;then use them to call the func
+  
+  ;If car of args is a pair and not empty, call addToEnv on it, then call evalBinding on the rest.
+  
+)
+
+;Takes myPairs (a pair or list of pairs) and adds it to env (an empty list or list of pairs). Works!
+(define (addToEnv env myPairs)  ;Adds myPairs to the end of list env
+  (if (equal? env '())   ;If env is an empty list...
+      (if (list? (car myPairs))
+        myPairs          ;If myPairs list of pairs, return the list.
+        (list myPairs)   ;If myPairs only one pair, put it in a list and return it.
       )
-;/ ' (needs work)
-  (if (equal? (car prog) '\')   ;This one's not quite working
-  (quote (prog))
- (cons '"FOOL! THE CHAR " (cons (car prog) '" WASN'T DEFINED!"));If the first character wasn't any of the defined functions
-);end of car is '
-);End of car is if
-);End of car is equal?
-);End of car is #f
-);End of car is #t
-);end of car is /
-);end of car is *
-);end of if car is -   
-);end of if car is +
-  prog ;If it's not a pair, return the program using default Racket.
-);End of if prog is a pair
-);End of whole function
+      
+      (if (list? (car myPairs)) ;If env is not empty...
+        (append env myPairs)          ;If myPairs is a list of pairs, append it to env.
+        (append env (list myPairs))   ;If myPairs is just one pair, put it in a list and append it to env.
+      )
+  )
+)
+;TEST RESULTS OF addToEnv
+  ;(addToEnv '() '(x 3))
+  ;(addToEnv '((x 3)) '(y 4))
+  ;(addToEnv '((x 3) (z 5)) '((y 4) (w 6)))
+  ;(addToEnv '() '((y 4) (w 6)))
+ ;  ^All 4 of these currently return () with 1 or more pairs in it. Perfect.
+
+;Makes a brand new environment from a list env
+(define (makeEnv env)  
+  (addToEnv '() env)
+)
+
+;Function to make a name-value pair. Currently unused.
+(define (makeVar id val)
+    (list id val)
+)
 
 
-;Empty frame syntax: (startEval '())
+;; Arithmetic operators
+(define (evalArithmetic expr env)
+    (let* ([op (car expr)]
+        [left (evalExpr (second expr) env)]
+        [right (evalExpr (third expr) env)])
+    (case op
+        [(+) (+ left right)]
+        [(-) (- left right)]
+        [(*) (* left right)]
+        [(/) (/ left right)]
+        [else (error "Unknown operator: " op)])))
+
+;; Relational operators
+(define (evalRelational expr env)
+    (let* ([op (car expr)]
+        [left (evalExpr (second expr) env)]
+        [right (evalExpr (third expr) env)])
+    (case op
+        [(=) (= left right)]
+        [(<=) (<= left right)]
+        [(<) (< left right)]
+        [(>=) (>= left right)]
+        [(>) (> left right)]
+        [(equal?) (equal? left right)]
+        [else (error "Unknown operator: " op)])))
+
+;; Lists
+(define (evalList expr env)
+    (let* ([op (car expr)]
+        [arg (evalExpr (second expr) env)])
+    (case op
+        [(car) (car arg)]
+        [(cdr) (cdr arg)]
+        [(cons) (cons arg (evalExpr (third expr) env))]
+        [(pair?) (pair? arg)]
+        [else (error "Unknown operator: " op)])))
+
+;; Conditional
+(define (evalIf expr env)
+    (let* ([cond (evalExpr (second expr) env)]
+        [then (evalExpr (third expr) env)]
+        [else (evalExpr (fourth expr) env)])
+    (if cond then else)))
+
+;; Lambda expression
+(define (makeLambda params body env)
+    (list 'lambda params body env))
+
+;; Function application
+(define (apply func args env)
+    (let* ([funcVal (evalExpr func env)]
+        [argVals (map (lambda (arg) (evalExpr arg env)) args)])
+    (applyFunc funcVal argVals)))
+
+(define (applyFunc func args)
+    (match func
+        [(list 'lambda params body env)
+            (let ([newEnv (append (map cons params args) env)])
+                (evalExpr body newEnv))]
+        [else (error "Unknown function: " func)]))
+
+
+
+;; Test arithmetic
+;(print
+;    (startEval '(+ 1 2))
+;)
+
+;; Test relational
+;(print
+;    (startEval '(= 1 1))
+;)
+
+;; Test lists
+;(print
+;    (startEval '(cons 1 '()))
+;)
+
+;; Test if
+;(print
+;    (startEval '(if (equal? 1 1) 1 2))
+;)
+
+;; Test lambda
+;(print
+   ;(startEval
+  ;  '(letrec ((fact
+    ;    (lambda (x)
+    ;        (if (= x 0) (quote 1)
+      ;          (* x (fact (= x 1)))))))
+      ;          (fact 10))))
+
+;; Test lambda eRIK'S TEST
+(print
+   (startEval
+    '(
+       (lambda (x y) (+ x y)) 10 20))
+       )
